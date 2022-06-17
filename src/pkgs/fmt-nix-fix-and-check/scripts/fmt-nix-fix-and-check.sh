b62589ec507d4f904cd1a6ac5b1f0486e1f7bb51
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2022-06-17 14:50:03 -0300

    Removes "$@"  and adds TODO about it

--- src/pkgs/fmt-nix-fix-and-check/scripts/fmt-nix-fix-and-check.sh
+++ src/pkgs/fmt-nix-fix-and-check/scripts/fmt-nix-fix-and-check.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 
+
+# TODO: adds some logic that allow not passing any args
+#  and it defaults to current directory.
+
 ./fmt-nix-fix.sh
 
-./fmt-nix-check.sh "$@"
+./fmt-nix-check.sh '.'
 
 exit 0
